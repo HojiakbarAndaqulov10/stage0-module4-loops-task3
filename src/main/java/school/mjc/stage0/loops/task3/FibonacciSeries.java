6c689c0a2221ecc8c84fa1cb20e0519c312bd819
Your Name <you@example.com>
2023-12-24 20:51:23 +0500

    thank you for your lesson

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,14 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
-        if (lastFibonacci <= 0) {
+        /*if (lastFibonacci <= 0) {
             System.out.println("Invalid input. Please enter a positive integer.");
             return;
-        }
+        }*/
 
         int first = 0, second = 1;
 
-        System.out.println(lastFibonacci);
 
         for (int i = 0; i < lastFibonacci; i++) {
             System.out.println(first);
@@ -17,6 +16,11 @@ public class FibonacciSeries {
             first = second;
             second = next;
         }
+        System.out.println(lastFibonacci);
     }
+
+  /*  public static void main(String[] args) {
+        printFibonacci(5);
+    }*/
 }
 
